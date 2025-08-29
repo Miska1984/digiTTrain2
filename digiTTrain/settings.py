f581c5098541da780459ae202f34ca75720353e9
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-08-29 16:48:14 +0000

    lépés 12

--- digiTTrain/settings.py
+++ digiTTrain/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gd_25j*vos)xlnio1ja2m=ne=z18z)nt3f1@u@$l%(t!uif9uk"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-gd_25j*vos)xlnio1ja2m=ne=z18z)nt3f1@u@$l%(t!uif9uk")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
