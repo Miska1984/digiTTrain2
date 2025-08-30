001b876fc9dd1455b6713f01d3edbf569bb0021c
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-08-30 18:14:23 +0000

    Lépés 20

--- digiTTrain/production.py
+++ digiTTrain/production.py
@@ -9,10 +9,13 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),  # Cloud SQL Unix socket
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
