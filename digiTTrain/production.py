1c03e82de09f9b13a27a9a01972d6eeefcb8917f
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-01 08:45:05 +0000

    Cloud SQL connet10

--- digiTTrain/production.py
+++ digiTTrain/production.py
@@ -2,17 +2,18 @@
 from .settings import *
 import os
 
-# Cloud Run URL-je a "Bad Request (400)" hiba elkerülésére
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')] 
+# Ha nincs ALLOWED_HOSTS beállítva, akkor ne legyen None
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 
-# Adatbázis beállításai a Cloud SQL-hez, környezeti változókból
+# Adatbázis beállításai Cloud SQL-hez (socket vagy TCP)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),  # Cloud SQL Unix socket
+        # Cloud SQL unix socket elérési út
+        'HOST': os.getenv('DB_HOST', f"/cloudsql/{os.getenv('CLOUDSQL_CONNECTION_NAME')}"),
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
