1d418068f94b7dcfd30fe2ef1d1b5ec72a97389d
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-03 14:21:34 +0000

    2025-09-03-8

--- digiTTrain/development.py
+++ digiTTrain/development.py
@@ -1,17 +1,23 @@
 # digiTTrain/development.py
 from .settings import *
 
-# Helyi MySQL adatbázis beállításai
-from .settings import *
-
-# Helyi MySQL adatbázis beállításai
+# Django adatbázis beállítások a Docker Compose-hoz
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'digittrain2',
         'USER': 'digittrain_user',
         'PASSWORD': 'password',
-        'HOST': 'mysql-db',
+        'HOST': 'mysql-db', # a docker-compose.yml service neve
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+# Engedélyezd a DEBUG módot a helyi fejlesztéshez
+DEBUG = True
+
+# Engedélyezd a lokális hosztokat
+ALLOWED_HOSTS = ['*']
