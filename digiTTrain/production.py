9d9cf631efc90c06ec0fe2509dee17bae4c2f12c
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-06 12:29:44 +0000

    2025-09-06-16

--- digiTTrain/production.py
+++ digiTTrain/production.py
@@ -3,38 +3,6 @@ import os
 from .settings import *
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': 'format',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': 'format',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'storages': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 
