46b878e2b9829261fdd925f3199ca13902789ce9
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-06 11:44:04 +0000

    2025-09-06-8

--- digiTTrain/settings.py
+++ digiTTrain/settings.py
@@ -137,36 +137,3 @@ LOGIN_REDIRECT_URL = 'core:main_page'
 # Egyedi User modell
 AUTH_USER_MODEL = "users.User"
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': 'format',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': 'format',
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG', # Állítsd DEBUG-ra a részletes naplózáshoz
-            'propagate': True,
-        },
-        # Ezen kívül érdemes lehet a storages backend naplózását is növelni
-        'storages': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
