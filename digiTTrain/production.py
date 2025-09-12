05da8c195a149a22ba5d716054bc26b9aa53f5cf
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-12 14:06:46 +0000

    2025-09-12-9

--- digiTTrain/production.py
+++ digiTTrain/production.py
@@ -30,20 +30,57 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # ===== GOOGLE CLOUD STORAGE BEÁLLÍTÁSOK =====
+# FONTOS: Két külön storage backend kell!
 DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
 STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
 
 GS_BUCKET_NAME = os.environ.get("GS_BUCKET_NAME", "digittrain-media-publikus-miska1984")
 GS_PROJECT_ID = os.environ.get("GS_PROJECT_ID", "digittrain-projekt")
 
+# Hitelesítés beállítása
+GS_AUTO_CREATE_BUCKET = False
 GS_DEFAULT_ACL = "publicRead"
 GS_QUERYSTRING_AUTH = False
 GS_FILE_OVERWRITE = False
 GS_MAX_MEMORY_SIZE = 1024 * 1024 * 5  # 5MB
 
+# Location beállítás
+GS_LOCATION = os.environ.get("GS_LOCATION", "europe-west1")
+
+# KRITIKUS: Custom storage osztályok definiálása
+GS_MEDIA_BUCKET_NAME = GS_BUCKET_NAME
+GS_STATIC_BUCKET_NAME = GS_BUCKET_NAME
+
+# URL-ek pontos beállítása
 MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
 STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/static/"
 
-# Állítsa be a STATIC_ROOT-ot egy ideiglenes mappára
+# Ideiglenes mappák (collectstatic-hez)
 STATIC_ROOT = "/app/staticfiles_temp"
 MEDIA_ROOT = "/app/mediafiles_temp"
+
+# Részletes logging a hibakereséshez
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('storages')
+logger.setLevel(logging.DEBUG)
+
+# Storage backend explicit importálása
+from storages.backends import gcloud
+
+# Custom storage class a jobb kontroll érdekében
+class MediaStorage(gcloud.GoogleCloudStorage):
+    bucket_name = GS_BUCKET_NAME
+    default_acl = 'publicRead'
+    querystring_auth = False
+    location = ''  # Ne legyen prefix a media fájlokhoz
+
+class StaticStorage(gcloud.GoogleCloudStorage):
+    bucket_name = GS_BUCKET_NAME
+    default_acl = 'publicRead' 
+    querystring_auth = False
+    location = 'static'  # Static fájlok a /static/ mappába
+
+# Storage backend újradefiniálása
+DEFAULT_FILE_STORAGE = 'digiTTrain.production.MediaStorage'
+STATICFILES_STORAGE = 'digiTTrain.production.StaticStorage'
