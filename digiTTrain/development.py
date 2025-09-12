999d8a1615c5a69fd3ca240b5f58758e98e82ad5
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-09-12 10:29:46 +0000

    2025-09-12

--- digiTTrain/development.py
+++ digiTTrain/development.py
@@ -1,19 +1,16 @@
 # digiTTrain/development.py
-from .settings import *
 import os
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Django adatbázis beállítások a Docker Compose-hoz
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'digittrain2',
         'USER': 'digittrain_user',
         'PASSWORD': 'password',
-        'HOST': 'mysql-db', # a docker-compose.yml service neve
+        'HOST': 'mysql-db',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
@@ -21,11 +18,8 @@ DATABASES = {
     }
 }
 
-# Engedélyezd a DEBUG módot a helyi fejlesztéshez
 DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
-# Engedélyezd a lokális hosztokat
-ALLOWED_HOSTS = ['*']
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
--- digiTTrain/settings.py
+++ digiTTrain/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'crispy_forms',
     'crispy_bootstrap5',
+    "storages",
     'core',
     'biometric_data',
 ]
