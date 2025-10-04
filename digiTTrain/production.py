182f85faeea58e7e45f5d2fce962f333bdc4a46d
Miska1984 <155302142+Miska1984@users.noreply.github.com>
2025-10-04 16:25:17 +0000

    2025-10-04-13

--- digiTTrain/production.py
+++ digiTTrain/production.py
@@ -31,6 +31,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     'https://digit-train-web-195803356854.europe-west1.run.app'
+    'https://digit-train.hu',  
+    'https://www.digit-train.hu' 
 ]
 
 # ===== GOOGLE CLOUD STORAGE BEÁLLÍTÁSOK =====
